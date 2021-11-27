6ac04a90db90710193c0551f800efcae7c6761c8
soheb alam <sohebalam@hotmail.com>
2021-11-27 11:31:16 +0000

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { Provider } from "next-auth/client"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <Provider session={session}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
-
-export default MyApp
